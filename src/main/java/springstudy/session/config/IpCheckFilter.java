cfadcbb4726b89eb96750353456d408dd636dda7
worldeddt <ktest92@gmail.com>
2024-10-28 19:39:57 +0900

    fix: 로그인 수정

--- src/main/java/springstudy/session/config/IpCheckFilter.java
+++ src/main/java/springstudy/session/config/IpCheckFilter.java
@@ -1,5 +1,6 @@
 package springstudy.session.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 import jakarta.servlet.FilterChain;
@@ -9,6 +10,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
 
+@Slf4j
 public class IpCheckFilter extends OncePerRequestFilter {
 
     @Override
@@ -21,6 +23,9 @@ public class IpCheckFilter extends OncePerRequestFilter {
         if (session != null && SecurityContextHolder.getContext().getAuthentication() != null) {
             String sessionIp = (String) session.getAttribute("ipAddress");
             String currentIp = request.getRemoteAddr();
+
+            log.debug("sessionIp: {}", sessionIp);
+            log.debug("currentIp: {}", currentIp);
 
             if (sessionIp != null && !sessionIp.equals(currentIp)) {
                 // IP 주소가 다르면 세션 무효화 및 로그아웃 처리
