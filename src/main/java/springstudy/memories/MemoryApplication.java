01823875a069adb313314099597b56148892ebe3
worldeddt <ktest92@gmail.com>
2024-11-18 07:46:46 +0900

    feat: memory

---
+++ src/main/java/springstudy/memories/MemoryApplication.java
@@ -0,0 +1,17 @@
+package springstudy.memories;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springstudy.jpa.JpaApplication;
+
+import java.awt.image.MemoryImageSource;
+
+@Slf4j
+@SpringBootApplication
+public class MemoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemoryImageSource.class, args);
+    }
+}
