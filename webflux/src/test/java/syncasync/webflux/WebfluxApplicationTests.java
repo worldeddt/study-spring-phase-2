1a83d7ec1bb33d616aca008e04cd2e16f9e474ee
worldeddt <ktest92@gmail.com>
2024-12-22 02:04:18 +0900

    feat: webflux service

--- webflux/src/test/java/syncasync/webflux/WebfluxApplicationTests.java
+++ webflux/src/test/java/syncasync/webflux/WebfluxApplicationTests.java
@@ -2,12 +2,32 @@ package syncasync.webflux;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.test.StepVerifier;
+import syncasync.webflux.services.WebFluxService;
 
 @SpringBootTest
 class WebfluxApplicationTests {
 
+	private WebFluxService webFluxService = new WebFluxService();
+
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	void startFluxTest() {
+		StepVerifier.create(webFluxService.startFlux())
+				.expectNext(1,2,3,4,5,6,7,8,9,10)
+				.verifyComplete();
+	}
+
+	@Test
+	void fluxCountTest() {
+		StepVerifier.create(webFluxService.fluxCount())
+				.expectNext(10L)
+				.verifyComplete();
+	}
+
+
 
 }
