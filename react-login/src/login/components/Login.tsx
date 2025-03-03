7007af552fe1cf7a883d98b92921998e179a6163
worldeddt <ktest92@gmail.com>
2025-03-04 02:18:45 +0900

    first vite

--- react-login/src/login/components/Login.tsx
+++ react-login/src/login/components/Login.tsx
@@ -1,6 +1,21 @@
 
 
+type Login = {
+  username:string;
+  password:string
+}
 
+const Login = () => {
 
+  const LoginClick = () => {
 
-const Login
+  }
+  return (
+    <div>
+      <input type="text"/>
+      <button onClick={LoginClick}>카카오 로그인</button>
+    </div>
+  )
+}
+
+export default Login;
---
+++ react-login/src/login/components/LoginOut.tsx
@@ -0,0 +1,18 @@
+
+
+
+
+const LoginOut = () => {
+
+
+  return (
+    <div>
+      <button>
+        로그아웃
+      </button>
+    </div>
+  )
+}
+
+
+export default LoginOut;
