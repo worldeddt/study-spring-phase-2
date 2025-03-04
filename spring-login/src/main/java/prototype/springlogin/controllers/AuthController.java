76c2e4129109f8574144894d792aed541bccb1fb
worldeddt <=>
2025-03-04 17:55:36 +0900

    feat: login check

--- spring-login/src/main/java/prototype/springlogin/controllers/AuthController.java
+++ spring-login/src/main/java/prototype/springlogin/controllers/AuthController.java
@@ -4,13 +4,11 @@ package prototype.springlogin.controllers;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.*;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
@@ -87,5 +85,18 @@ public class AuthController {
         response.addCookie(cookie);
 
         return "로그아웃 성공";
+    }
+
+    @GetMapping("/check")
+    public boolean checkAuth(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("access_token".equals(cookie.getName())) {
+                    return true; // ✅ 로그인 상태 유지
+                }
+            }
+        }
+        return false; // ✅ 로그아웃 상태
     }
 }
